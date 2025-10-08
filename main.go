c2f9f4d2fff8fb3d0484b4f0a6a8824b6a96890b
bugracdnc <bugracaliskandinc@gmail.com>
2025-10-08 21:22:07 +0300

    Update main.go with temp entry points

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
-package worksheetparser
+package main
+
+import (
+	"log"
+
+	"worksheetparser/debug"
+	"worksheetparser/parser"
+)
+
+func main() {
+	worksheet, err := parser.Parse("examples/example.wsp.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	debug.LogPrintln("-------------------results-------------------")
+	debug.LogPrint(worksheet)
+}
