eafab635752200e46816de474eeb06251e8b9ddc
bugracdnc <bugracaliskandinc@gmail.com>
2025-10-08 22:11:51 +0300

    Update main.go with getting filepath from arguments logic

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 
 	"worksheetparser/debug"
 	"worksheetparser/generator"
@@ -9,7 +12,19 @@ import (
 )
 
 func main() {
-	worksheet, err := parser.Parse("examples/example.wsp.txt")
+	if len(os.Args) < 2 {
+		fmt.Println("Please drag and drop a worksheet file onto this executable")
+		fmt.Println("or run worksheetpager <filename> in a terminal")
+		return
+	}
+
+	inputPath := os.Args[1]
+	dir := filepath.Dir(inputPath)
+
+	interactiveOut := filepath.Join(dir, "interactive.html")
+	printableOut := filepath.Join(dir, "printable.html")
+
+	worksheet, err := parser.Parse(inputPath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -17,6 +32,15 @@ func main() {
 	debug.LogPrintln("-------------------results-------------------")
 	debug.LogPrint(worksheet)
 
-	generator.GenerateInteractive(worksheet, "interactive.html")
-	generator.GeneratePrintable(worksheet, "printable.html")
+	fmt.Println("Generating HTML worksheets...")
+
+	if err := generator.GenerateInteractive(worksheet, interactiveOut); err != nil {
+		fmt.Println("Error generating interactive version: ", err)
+	}
+
+	if err := generator.GeneratePrintable(worksheet, printableOut); err != nil {
+		fmt.Println("Error generating printable version: ", err)
+	}
+
+	fmt.Printf("Done generating files!\nCreated:\n - %s\n - %s\n", interactiveOut, printableOut)
 }
